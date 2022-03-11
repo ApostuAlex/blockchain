1c919de80af832cc0829890bbb5cd84d139ca2b2
ApostuAlex <74897876+ApostuAlex@users.noreply.github.com>
2022-03-11 16:20:31 +0200

    Add files via upload

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,104 +1,244 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const Blockchain = require('./blockchain');
-const uuid = require('uuid');
+const bodyParser = require("body-parser");
+const Blockchain = require("./blockchain");
+const uniqueID = require("uuid");
 const port = process.argv[2];
-const nodeAddress = uuid.v1().split('-').join('');
-const rp = require('request-promise');
-const { request } = require('express');
+const nodeAddress = uniqueID.v1().split("-").join("");
+const rp = require("request-promise");
+const { request } = require("express");
 
 const ftm = new Blockchain();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/blockchain", function (req, res) {
+  res.send(ftm);
+});
 
+app.post("/transaction", function (req, res) {
+  const newTransaction = req.body;
+  const blockIndex = ftm.addTransactionToPendingTransactions(newTransaction);
+  res.json({
+    note: `The new transaction was added in the block with the index ${blockIndex}`,
+  });
+});
 
-app.get('/blockchain', function (req, res) {
-    res.send(ftm);
+app.post("/transaction/broadcast", function (req, res) {
+  const newTransaction = ftm.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+  ftm.addTransactionToPendingTransactions(newTransaction);
+
+  const requestPromises = [];
+  ftm.networkNodes.forEach((networkNodeUrl) => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/transaction",
+      method: "POST",
+      body: newTransaction,
+      json: true,
+    };
+    requestPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(requestPromises).then((data) => {
+    res.json({ note: "Transaction created and broadcast successfully" });
+  });
+});
+
+app.post("/receive-new-block", function (req, res) {
+  const newBlock = req.body.newBlock;
+  const lastBlock = ftm.getLastBlock();
+  const correctHash = lastBlock.hash === newBlock.previousBlockHash;
+  const correctIndex = lastblock['index'] + 1 === newBlock['index'];
+
+  if (correctHash && correctIndex) {
+    ftm.chain.push(newBlock);
+    ftm.pendingTransactions = [];
+    res.json({
+      note: "New block was accepted",
+      newBlock: newBlock
+    });
+  }
+  else {
+    res.json({
+      note: "New block rejected!",
+      newBlock: newBlock
+    })
+  }
 })
 
+app.get("/mine", function (req, res) {
+  const lastBlock = ftm.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+  const currentBlockData = {
+    transactions: ftm.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
 
-app.post('/transaction', function (req, res) {
-    const blockIndex = ftm.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    res.json({ note: `Transaction will be added in the block ${blockIndex}.` });
-})
+  const nonce = ftm.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = ftm.hashBlock(previousBlockHash, currentBlockData, nonce);
+  const newBlock = ftm.createNewBlock(nonce, previousBlockHash, blockHash);
 
+  const requestPromises = [];
+  ftm.networkNodes.forEach((networkNodeUrl) => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/receive-new-block",
+      method: "POST",
+      body: { newBlock: newBlock },
+      json: true,
+    };
+    requestPromises.push(rp(requestOptions));
+  });
 
-app.get('/mine', function (req, res) {
-    const lastBlock = ftm.getLastBlock();
-    const previousBlockHash = lastBlock['hash'];
-    const currentBlockData = {
-        transactions: ftm.pendingTransactions,
-        index: lastBlock['index'] + 1
+  Promise.all(requestPromises)
+    .then((data) => {
+      const requestOptions = {
+        uri: ftm.currentNodeUrl + "/transaction/broadcast",
+        method: "POST",
+        body: {
+          amount: 420,
+          sender: "00dasdadasdadwawds",
+          recipient: nodeAddress,
+        },
+        json: true,
+      };
+      return rp(requestOptions);
+    })
+    .then((data) => {
+      res.json({
+        note: `New block mined successfully `,
+        block: newBlock,
+      });
+    });
+});
+
+app.post("/register-and-broadcast-node", function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl;
+  if (ftm.networkNodes.indexOf(newNodeUrl) == -1) ftm.networkNodes.push(newNodeUrl);
+
+  const regNodesPromises = [];
+  ftm.networkNodes.forEach((networkNodeUrl) => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/register-node",
+      method: "POST",
+      body: { newNodeUrl: newNodeUrl },
+      json: true,
     };
 
-    const nonce = ftm.proofOfWork(previousBlockHash, currentBlockData);
-    const blockHash = ftm.hashBlock(previousBlockHash, currentBlockData, nonce);
+    regNodesPromises.push(rp(requestOptions));
+  });
 
-    ftm.createNewTransaction(420, "00", nodeAddress);
+  Promise.all(regNodesPromises)
+    .then((data) => {
+      const bulkRequestOptions = {
+        uri: newNodeUrl + "/register-nodes-bulk",
+        method: "POST",
+        body: { allNetworkNodes: [...ftm.networkNodes, ftm.currentNodeUrl] },
+        json: true,
+      };
 
-    const newBlock = ftm.createNewBlock(nonce, previousBlockHash, blockHash);
-
-
-
-    res.json({
-        note: `New block mined successfully `,
-        block: newBlock
-    });
-})
-
-app.post('/register-and-broadcast-node', function (req, res) {
-
-    const newNodeUrl = req.body.newNodeUrl;
-    if (ftm.networkNodes.indexOf(newNodeUrl) == -1)
-        ftm.networkNodes.push(newNodeUrl);
-
-    const regNodesPromises = [];
-    ftm.newtworkNodes.forEach(networtNodeUrl => {
-        const requestOptions = {
-            uri: networkNodeUrl + '/register-node',
-            method: 'POST',
-            body: { newNodeUrl: newNodeUrl },
-            json: true
-        };
-
-        regNodesPromises.push(rp(requestOptions));
-
-        Promise.all(regNodesPromises)
-            .then(data => {
-                const bulkRequestOptions = {
-                    uri: networtNodeUrl + '/register-nodes-bulk',
-                    method: 'POST',
-                    body: { allNetworkNodes: [...ftm.networkNodes, ftm.currentNodeUrl] },
-                    json: true
-                };
-
-                return rp(bulkRequestOptions);
-            })
-            .then(data => {
-                res.json = {
-                    note: 'New node register successfully with the network'
-                };
-            })
+      return rp(bulkRequestOptions);
     })
-
+    .then((data) => {
+      res.json({ note: "New node registered with network" });
+    });
 });
 
-app.post('/register-node', function (req, res) {
-    const newNodeUrl = req.body.newNodeUrl;
-    const nodeNotAlreadyPresent = ftm.networkNodes.indexOf(newNodeUrl) == -1;
-    const notCurrentNode = ftm.currentNodeUrl !== newNodeUrl;
-    if (nodeNotAlreadyPresent && notCurrentNode) ftm.networkNodes.push(newNodeUrl);
-    res.json = { note: 'New node register successfully' }
+app.post("/register-node", function (req, res) {
+  const newNodeUrl = req.body.newNodeUrl;
+  const nodeNotAlreadyPresent = ftm.networkNodes.indexOf(newNodeUrl) == -1;
+  const notCurrentNode = ftm.currentNodeUrl !== newNodeUrl;
+  if (nodeNotAlreadyPresent && notCurrentNode) ftm.networkNodes.push(newNodeUrl);
+  res.json({ note: "New node register successfully" });
 });
 
-app.post('/register-nodes-bulk', function (req, res) {
-
+app.post("/register-nodes-bulk", function (req, res) {
+  const allNetworkNodes = req.body.allNetworkNodes;
+  allNetworkNodes.forEach((networkNodeUrl) => {
+    const nodeNotAlreadyPresent =
+      ftm.networkNodes.indexOf(networkNodeUrl) == -1;
+    const notCurrentNode = ftm.currentNodeUrl !== networkNodeUrl;
+    if (nodeNotAlreadyPresent && notCurrentNode)
+      ftm.networkNodes.push(networkNodeUrl);
+  });
+  res.json({ note: "Bulk of the nodes is successful" });
 });
 
+app.get("/consensus", function (req, res) {
+  const requestPromises = [];
+  ftm.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/blockchain",
+      method: "GET",
+      json: true
+    }
+    requestPromises.push(rp(requestOptions));
+  })
+
+  Promise.all(requestPromises)
+    .then(blockchains => {
+      const currentChainLength = ftm.chain.length;
+      const maxChainLength = currentChainLength;
+      const newLongestChain = null;
+      const newPendingTransactions = null;
+
+      blockchains.forEach(blockchain => {
+        if (blockchain.chain.length > maxChainLength) {
+          maxChainLength = blockchain.chain.length;
+          newLongestChain = blockchain.chain;
+          newPendingTransactions = blockchain.pendingTransactions;
+        }
+      })
+
+      if (!newLongestChain || (newLongestChain && !ftm.chainIsValid(newLongestChain))) {
+        res.json({
+          note: "Chain has not been replaced with another one",
+          chain: ftm.chain
+        });
+      }
+
+      else if (newLongestChain && ftm.chainIsValid(newLongestChain)) {
+        ftm.chain = newLongestChain;
+        ftm.pendingTransactions = newPendingTransactions;
+        res.json({
+          note: "Chain has been replaced with another chain which has a long lenght, so more proof of work has been done",
+          chain: ftm.chain
+        });
+      }
+    })
+});
+
+app.get('/block/:blockHash', function (req, res) {
+  const blockHash = req.params.blockHash;
+  const correctBlock = ftm.getBlock(blockHash);
+  res.json({
+    block: correctBlock
+  });
+});
+
+
+app.get('/transaction/:transactionID', function (req, res) {
+  const transactionID = req.params.transactionID;
+  const transactionData = ftm.getTransaction(transactionID);
+  res.json({
+    transaction: transactionData.transaction,
+    block: transactionData.block
+  })
+});
+
+
+app.get('/address/:address', function (req, res) {
+  const address = req.params.address;
+  const addressData = ftm.getAddressData(address);
+  res.json({
+    addressData: addressData
+  })
+});
+  
 app.listen(port, function () {
-    console.log(`Listening on port ${port}...`)
+  console.log(`Listening on port ${port}...`);
 });
--- dev/test.js
+++ dev/test.js
@@ -1,34 +1,101 @@
 const Blockchain = require('./blockchain');
-
 const ftm = new Blockchain();
 
-// ftm.createNewBlock(2393, "0908", "009s");
-// ftm.createNewTransaction('100ftm', '09das9ds0da', '420dasda2asdasd'); 
-// ftm.createNewBlock(231231, "9939292292", "2312312312"); 
+const ftm1 = {
+    "chain": [
+        {
+            "index": 1,
+            "timestamp": 1646810095285,
+            "transactions": [
+
+            ],
+            "nonce": 1200,
+            "hash": "asda2wscvc",
+            "previousBlockHash": "ASD@@SCZXCZX"
+        },
+        {
+            "index": 2,
+            "timestamp": 1646810160595,
+            "transactions": [
+
+            ],
+            "nonce": 17378,
+            "hash": "0000364be76329b014a3fe7da0e876d2ee9227d23f3648309db8444e48bd1571",
+            "previousBlockHash": "asda2wscvc"
+        },
+        {
+            "index": 3,
+            "timestamp": 1646810313990,
+            "transactions": [
+                {
+                    "amount": 420,
+                    "sender": "00dasdadasdadwawds",
+                    "recipient": "9ecdc4a09f7811ec96f6a1c8dff50f8e",
+                    "transactionID": "c5de60e09f7811ec96f6a1c8dff50f8e"
+                },
+                {
+                    "amount": 33,
+                    "sender": "DASDWDASACASdasdaca",
+                    "recipient": "0000cdscsdcsdcasdas2sd",
+                    "transactionID": "f703b5309f7811ec96f6a1c8dff50f8e"
+                },
+                {
+                    "amount": 40,
+                    "sender": "DASDWDASACASdasdaca",
+                    "recipient": "0000cdscsdcsdcasdas2sd",
+                    "transactionID": "fa9e68209f7811ec96f6a1c8dff50f8e"
+                }
+            ],
+            "nonce": 17968,
+            "hash": "0000c4bb9e89b3c9ac81f62753dcff8939b3f67372429a359ce2de9b14c1db93",
+            "previousBlockHash": "0000364be76329b014a3fe7da0e876d2ee9227d23f3648309db8444e48bd1571"
+        },
+        {
+            "index": 4,
+            "timestamp": 1646810432562,
+            "transactions": [
+                {
+                    "amount": 420,
+                    "sender": "00dasdadasdadwawds",
+                    "recipient": "9ecdc4a09f7811ec96f6a1c8dff50f8e",
+                    "transactionID": "214964c09f7911ec96f6a1c8dff50f8e"
+                }
+            ],
+            "nonce": 69784,
+            "hash": "0000d71ca1f55644c637106e82de0ec31a8e8dfdb13945f2b18d22ae29d413a4",
+            "previousBlockHash": "0000c4bb9e89b3c9ac81f62753dcff8939b3f67372429a359ce2de9b14c1db93"
+        },
+        {
+            "index": 5,
+            "timestamp": 1646810435621,
+            "transactions": [
+                {
+                    "amount": 420,
+                    "sender": "00dasdadasdadwawds",
+                    "recipient": "9ecdc4a09f7811ec96f6a1c8dff50f8e",
+                    "transactionID": "67f5bd609f7911ec96f6a1c8dff50f8e"
+                }
+            ],
+            "nonce": 39059,
+            "hash": "0000b11a9642397b14731493182c3b6588774f6cad95b0c95fb0918487b7b921",
+            "previousBlockHash": "0000d71ca1f55644c637106e82de0ec31a8e8dfdb13945f2b18d22ae29d413a4"
+        }
+    ],
+    "pendingTransactions": [
+        {
+            "amount": 420,
+            "sender": "00dasdadasdadwawds",
+            "recipient": "9ecdc4a09f7811ec96f6a1c8dff50f8e",
+            "transactionID": "69c85a809f7911ec96f6a1c8dff50f8e"
+        }
+    ],
+    "currentNodeUrl": "http://localhost:3001",
+    "networkNodes": [
+
+    ]
+}
 
 
-const previousBlockHash = "099das0dsadasdwd21sadadasdasda";
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: "#@#!",
-        recipient: "ADASDas1"
-    },
-    {
-        amount: 10,
-        sender: "DASDSACAXZCCZXC",
-        recipient: "DASD721EDSBUDASN"
-    },
-    {
-        amount: 10,
-        sender: "GFGDGDGSDGS",
-        recipient: "DASNDASDNASDSAJDASNDA"
-    }
-];
-
-
-
-console.log(ftm.hashBlock(previousBlockHash, currentBlockData,30328));
-
+console.log(ftm.chainIsValid(ftm1.chain));
 
 
